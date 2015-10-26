998aef31cb3c0a2e5fb46e4d01fdbdb535fa867c
chepee73 <chepee73@localhost.localdomain>
2015-10-26 13:49:22 -0300

    Fixed World.rb

--- world.rb
+++ world.rb
@@ -3,22 +3,22 @@ require 'stringio'
 class World
   attr_accessor :world, :first_room
   @world = {}
+  def self.world
+    @world
+  end
   class << self
-
     def load_tiles(name)
       rows = []
       File.open("resources/#{name}.txt", "r") {|file| rows = file.readlines}
-      rows_length = rows[0].split("\t").length
+      rows_length = (rows[0].split("\t")).length
       rows.each_with_index do |row, y|
         cols = rows[y].split("\t")
         rows_length.times do |x|
-          room_name = cols.gsub("\n", "")
-          if room_name == "\t"
+          room_name = cols[x].gsub("\n", '') if cols[x]
+          if room_name == ""
             @world[x => y] = nil
           else
-            print "X: #{x} Y: #{y}\n"
             @world[x => y] = room_name
-            print @world[x => y]
           end
         end
       end
@@ -27,3 +27,4 @@ class World
 end
 
 World.load_tiles("ship")
+puts World.world
